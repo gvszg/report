cd4d950be6d196614f1be6aaae4d51f3826e0d04
jamesyu <zetaentranceserious@gmail.com>
2015-09-22 19:16:03 +0800

    take break

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,44 @@
 class StudentsController < ApplicationController
+  before_action :find_gradeclass
+
   def index
-    @gradeclass = Gradeclass.find(params[:gradeclass_id])
     @students = @gradeclass.students.all
+    @student = @students.where(params[:id]).first
+    @scores = @student.scores
+    @score = @scores.where(params[:id]).first.score
+    
+  end
+
+  def show
+    @student = @students.where(params[:id]).first
+    @scores = @student.scores
+    @score = @scores.where(params[:id]).first.score
+  end
+
+  def new
+    @student = @gradeclass.students.new
+  end
+
+  def create
+    @student = @gradeclass.students.new(student_params)
+
+    if @student.save
+      flash[:notice] = "新增學生成功"
+      redirect_to gradeclass_students_path(@gradeclass)
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+
+  def student_params
+    params.require(:student).permit(:number, :name)  
+  end
+
+  def find_gradeclass
+    @gradeclass = Gradeclass.find(params[:gradeclass_id])
   end
 end
